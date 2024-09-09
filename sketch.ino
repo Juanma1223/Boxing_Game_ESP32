09c6459cf328b32d3c0b5c529ffb67fd952b1336
Juanma Fernandez <juanmanuelf12@gmail.com>
2024-09-09 16:59:38 -0300

    add interruptions

--- sketch.ino
+++ sketch.ino
@@ -5,48 +5,35 @@
 const uint8_t buttonPins[] = {4, 16, 12, 14};
 const uint8_t ledPins[] = {0, 17, 13, 27};
 const int POSITIONS_QUANTITY = 4;
-int buttonPressed = 0;
+int buttonPressed = -1;
 int currentPos = 0;
 // This is the time player has to punch the chosen position
 int waitTime = 2;
 
-bool outOfTime = false;
-
-// Timer handle
-esp_timer_handle_t timerHandle;
-
-// This function is called when the player is out of time to tap de piezoelectric
-void setOutOfTime(void* arg) {
-  outOfTime = true;
-  Serial.print("Out!");
+void IRAM_ATTR pushButton0() {
+  buttonPressed = 0;
 }
 
-void readButtons() {
-  while (!outOfTime) {
-    for (int i = 0; i < 4; i++) {
-      int buttonPin = buttonPins[i];
-      if (digitalRead(buttonPin) == LOW) {
-        if (i == currentPos) {
-          Serial.print("Correct");
-        } else {
-          Serial.print("Incorrect");
-        }
-        return;
-      }
-    }
-  }
+void IRAM_ATTR pushButton1() {
+  buttonPressed = 1;
+}
+
+void IRAM_ATTR pushButton2() {
+  buttonPressed = 2;
+}
+
+void IRAM_ATTR pushButton3() {
+  buttonPressed = 3;
 }
 
 // Return a random position for the user to push
 int randomPosition() {
   int buf = 0;
   esp_fill_random(&buf, sizeof(buf));
-  Serial.println(buf);
   int pos = buf % POSITIONS_QUANTITY;
   if (pos < 0) {
     pos = pos * -1;
   }
-  Serial.println(pos);
   return (pos);
 }
 
@@ -68,22 +55,20 @@ void setup()
     pinMode(ledPins[i], OUTPUT);
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
-    // Create and configure the timer
-    const esp_timer_create_args_t timerArgs = {
-        .callback = &setOutOfTime, // Callback function
-        .name = "setOutOfTime"        // Timer name
-    };
 
-    // Create the timer
-    esp_timer_create(&timerArgs, &timerHandle);
-
-    // Start the timer with a 1-second interval (1000000 microseconds)
-    esp_timer_start_periodic(timerHandle, 1000000); // 1,000,000 microseconds = 1 second
+  attachInterrupt(buttonPins[0], pushButton0, RISING);
+  attachInterrupt(buttonPins[1], pushButton1, RISING);
+  attachInterrupt(buttonPins[2], pushButton2, RISING);
+  attachInterrupt(buttonPins[3], pushButton3, RISING);
 }
 
 void loop() {
   blink();
-  readButtons();
-  outOfTime = false;
+  delay(waitTime * 1000);
   resetLeds();
+  if(buttonPressed == currentPos){
+    Serial.print("Correct");
+  }else{
+    Serial.print("Incorrect");
+  }
 }
