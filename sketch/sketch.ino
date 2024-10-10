eb1d70e8196e7113cbebb9459e0b645daaa59cf1
Juanma Fernandez <juanmanuelf12@gmail.com>
2024-10-09 21:07:46 -0300

    merge conflicts

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,15 +5,15 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
-const uint8_t buttonPins[] = {4, 16, 12, 14};
-const uint8_t ledPins[] = {0, 17, 13, 27};
+const uint8_t buttonPins[] = { 4, 16, 12, 14 };
+const uint8_t ledPins[] = { 0, 17, 13, 27 };
 const int POSITIONS_QUANTITY = 4;
 int buttonPressed = -1;
 int currentPos = 0;
 // This is the time player has to punch the chosen position
 int waitTime = 2;
 bool usePattern = true;
-int pattern[] = {0, 1, 2};
+int pattern[] = { 0, 1, 2 };
 int currentPatternPos = 0;
 
 const char *ssid = "Boxing game";
@@ -21,62 +21,47 @@ const char *password = "123456789";
 
 AsyncWebServer server(80);
 
-void IRAM_ATTR pushButton0()
-{
+void IRAM_ATTR pushButton0() {
   buttonPressed = 0;
 }
 
-void IRAM_ATTR pushButton1()
-{
+void IRAM_ATTR pushButton1() {
   buttonPressed = 1;
 }
 
-void IRAM_ATTR pushButton2()
-{
+void IRAM_ATTR pushButton2() {
   buttonPressed = 2;
 }
 
-void IRAM_ATTR pushButton3()
-{
+void IRAM_ATTR pushButton3() {
   buttonPressed = 3;
 }
 
 // Return a random position for the user to push
-int randomPosition()
-{
+int randomPosition() {
   int buf = 0;
   esp_fill_random(&buf, sizeof(buf));
   int pos = buf % POSITIONS_QUANTITY;
-  if (pos < 0)
-  {
+  if (pos < 0) {
     pos = pos * -1;
   }
   return (pos);
 }
 
-<<<<<<< HEAD
-void blink()
-{
-=======
 void blinkRandom() {
->>>>>>> dd7d08fdf5a434d79b15d402f2cdb1dbbc97dc4e
   currentPos = randomPosition();
   digitalWrite(ledPins[currentPos], HIGH);
 }
 
-void resetLeds()
-{
-  for (int i = 0; i < POSITIONS_QUANTITY; i++)
-  {
+void resetLeds() {
+  for (int i = 0; i < POSITIONS_QUANTITY; i++) {
     digitalWrite(ledPins[i], LOW);
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
-  for (int i = 0; i < 4; i++)
-  {
+  for (int i = 0; i < 4; i++) {
     pinMode(ledPins[i], OUTPUT);
     pinMode(buttonPins[i], INPUT_PULLUP);
   }
@@ -92,8 +77,7 @@ void setup()
   Serial.println(IP);
 
   server.on(
-      "/combination", HTTP_POST, [](AsyncWebServerRequest *request) {}, nullptr, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
-      {
+    "/combination", HTTP_POST, [](AsyncWebServerRequest *request) {}, nullptr, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
       StaticJsonDocument<200> jsonDoc;
       DeserializationError error = deserializeJson(jsonDoc, data);
 
@@ -108,11 +92,11 @@ void setup()
       response += jsonDoc["param2"].as<String>();
 
       Serial.println(response);
-      request->send(200, "application/json", "{\"status\":\"success\"}"); });
+      request->send(200, "application/json", "{\"status\":\"success\"}");
+    });
 
   server.on(
-      "/speed", HTTP_POST, [](AsyncWebServerRequest *request) {}, nullptr, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
-      {
+    "/speed", HTTP_POST, [](AsyncWebServerRequest *request) {}, nullptr, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
       StaticJsonDocument<200> jsonDoc;
       DeserializationError error = deserializeJson(jsonDoc, data);
 
@@ -125,24 +109,12 @@ void setup()
       response += jsonDoc["value"].as<String>();
 
       Serial.println(response);
-      request->send(200, "application/json", "{\"status\":\"success\"}"); });
+      request->send(200, "application/json", "{\"status\":\"success\"}");
+    });
 
   server.begin();
 }
 
-<<<<<<< HEAD
-void loop()
-{
-  blink();
-  delay(waitTime * 1000);
-  resetLeds();
-  if (buttonPressed == currentPos)
-  {
-    Serial.print("Correct");
-  }
-  else
-  {
-=======
 void loop() {
   if (!usePattern) {
     blinkRandom();
@@ -156,7 +128,6 @@ void loop() {
   if (buttonPressed == currentPos) {
     Serial.print("Correct");
   } else {
->>>>>>> dd7d08fdf5a434d79b15d402f2cdb1dbbc97dc4e
     Serial.print("Incorrect");
   }
 }
